0adf30830db97ebae393c4b40aa53ab53456c626
anlianaaguilar <anlianaaguilar@gmail.com>
2022-07-25 20:38:36 -0400

    se crea carpeta firebase

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCDMvd0c5bBXd5R-WmutM7JPNNEPusF2vw",
+    authDomain: "curso-alfaweb.firebaseapp.com",
+    projectId: "curso-alfaweb",
+    storageBucket: "curso-alfaweb.appspot.com",
+    messagingSenderId: "130639974223",
+    appId: "1:130639974223:web:7fd4dffe14d8edf4febf3a"
+};
+  
